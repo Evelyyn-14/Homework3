80d33d3a2e0459c09178a30aab018a9006e08414
Evelyynesco2014 <eescobedo1@student.gsu.edu>
2025-03-11 12:40:21 -0400

    Card Matching Game Features

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,175 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => GameState(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Card Matching Game',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CardMatchingGame(title: 'Card Matching Game'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class CardMatchingGame extends StatefulWidget {
+  const CardMatchingGame({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CardMatchingGame> createState() => _CardMatchingGameState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CardModel {
+  final String frontDesign;
+  final String backDesign;
+  bool isFaceUp;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  CardModel({
+    required this.frontDesign,
+    required this.backDesign,
+    this.isFaceUp = false,
+  });
+}
+
+class GameState extends ChangeNotifier {
+  List<CardModel> cards = List.generate(
+    16,
+    (index) => CardModel(
+      frontDesign: 'assets/images/card${index % 8 + 1}.jpg',
+      backDesign: 'assets/images/backofcard.jpg',
+    ),
+  )..shuffle();
+
+  List<int> _faceUpCards = [];
+
+  void flipCard(int index) {
+    if (_faceUpCards.length < 2) {
+      cards[index].isFaceUp = !cards[index].isFaceUp;
+      if (cards[index].isFaceUp) {
+        _faceUpCards.add(index);
+      } else {
+        _faceUpCards.remove(index);
+      }
+      notifyListeners();
+
+      if (_faceUpCards.length == 2) {
+        Future.delayed(const Duration(seconds: 1), () {
+          _checkMatch();
+        });
+      }
+    }
+  }
+
+  void _checkMatch() {
+    if (_faceUpCards.length == 2) {
+      final firstCard = cards[_faceUpCards[0]];
+      final secondCard = cards[_faceUpCards[1]];
+
+      if (firstCard.frontDesign != secondCard.frontDesign) {
+        firstCard.isFaceUp = false;
+        secondCard.isFaceUp = false;
+      }
+      _faceUpCards.clear();
+      notifyListeners();
+    }
+  }
+}
+
+class _CardMatchingGameState extends State<CardMatchingGame> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  int? _flippingCardIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final gameState = Provider.of<GameState>(context);
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
         ),
+        itemCount: gameState.cards.length,
+        itemBuilder: (context, index) {
+          final card = gameState.cards[index];
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                _flippingCardIndex = index;
+              });
+              gameState.flipCard(index);
+              _controller.forward(from: 0.0);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AnimatedBuilder(
+                animation: _animation,
+                builder: (context, child) {
+                  final isFlipping = _flippingCardIndex == index;
+                  final rotationValue = isFlipping ? _animation.value * 3.14 : 0.0;
+                  return Transform(
+                    transform: Matrix4.rotationY(rotationValue),
+                    alignment: Alignment.center,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(color: Colors.black),
+                        image: card.isFaceUp
+                            ? DecorationImage(
+                                image: AssetImage(card.frontDesign),
+                                fit: BoxFit.cover,
+                              )
+                            : DecorationImage(
+                                image: AssetImage(card.backDesign),
+                                fit: BoxFit.cover,
+                              ),
+                      ),
+                      child: Center(
+                        child: card.isFaceUp ? Container() : Container(),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
